c0a55711b03762863c51bed03077eb9343a0d686
Jeff Emanuel <jeff141421@gmail.com>
2026-04-19 21:05:59 -0400

    test: extend escape parser serde proptests

--- frankenterm/escape-parser/tests/proptest_escape_serde.rs
+++ frankenterm/escape-parser/tests/proptest_escape_serde.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "use_serde")]
 
-use frankenterm_escape_parser::csi::Underline;
+use frankenterm_escape_parser::csi::{Blink, Underline, VerticalAlign};
 use frankenterm_escape_parser::hyperlink::Hyperlink;
 use proptest::prelude::*;
 use std::collections::HashMap;
@@ -46,6 +46,18 @@ fn arb_underline() -> impl Strategy<Value = Underline> {
     ]
 }
 
+fn arb_blink() -> impl Strategy<Value = Blink> {
+    prop_oneof![Just(Blink::None), Just(Blink::Slow), Just(Blink::Rapid),]
+}
+
+fn arb_vertical_align() -> impl Strategy<Value = VerticalAlign> {
+    prop_oneof![
+        Just(VerticalAlign::BaseLine),
+        Just(VerticalAlign::SuperScript),
+        Just(VerticalAlign::SubScript),
+    ]
+}
+
 proptest! {
     #![proptest_config(ProptestConfig::with_cases(128))]
 
@@ -60,6 +72,20 @@ proptest! {
     fn underline_json_roundtrip(value in arb_underline()) {
         let json = serde_json::to_string(&value).unwrap();
         let back: Underline = serde_json::from_str(&json).unwrap();
+        prop_assert_eq!(back, value);
+    }
+
+    #[test]
+    fn blink_json_roundtrip(value in arb_blink()) {
+        let json = serde_json::to_string(&value).unwrap();
+        let back: Blink = serde_json::from_str(&json).unwrap();
+        prop_assert_eq!(back, value);
+    }
+
+    #[test]
+    fn vertical_align_json_roundtrip(value in arb_vertical_align()) {
+        let json = serde_json::to_string(&value).unwrap();
+        let back: VerticalAlign = serde_json::from_str(&json).unwrap();
         prop_assert_eq!(back, value);
     }
 }
